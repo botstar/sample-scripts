8374cb5c52a23fb6ee4e0cfe349d1cfc7df8dbf0
Hai Le <37065046+botstar-hai@users.noreply.github.com>
2020-02-28 02:18:46 +0700

    Update extract-action.js

--- extract-latest-response/extract-action.js
+++ extract-latest-response/extract-action.js
@@ -2,9 +2,9 @@
 const latestResponse = event.conversation.userResponses.latest_response;
 // extract data from latest response, which is formatted: `varName:value|varName1:value1`
 const data = latestResponse
-	.split('|') // every value will be separated as an array of `varName:value`
-	.map(v => v.split(':')) // value will be an array of [varName, value] 
-	.reduce(
+  .split('|') // every value will be separated as an array of `varName:value`
+  .map(v => v.split(':')) // value will be an array of [varName, value] 
+  .reduce(
     (result, [varName, value]) => Object.assign(result, { [varName]: value }),
     {}
   );
