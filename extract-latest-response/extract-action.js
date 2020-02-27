8256ac411579d9188f046769a7cca382b97e4633
Hai Le <37065046+botstar-hai@users.noreply.github.com>
2020-02-28 02:21:19 +0700

    Update extract-action.js

--- extract-latest-response/extract-action.js
+++ extract-latest-response/extract-action.js
@@ -1,6 +1,7 @@
-// Extract and save data to variables
+// This script is used for extracting data from latest response,
+// which is formatted: `varName:value|varName1:value1`
+// and save `varName`, `varName1` values to the corresponding bot flow variable names
 const latestResponse = event.conversation.userResponses.latest_response;
-// extract data from latest response, which is formatted: `varName:value|varName1:value1`
 const data = latestResponse
   .split('|') // every value will be separated as an array of `varName:value`
   .map(v => v.split(':')) // value will be an array of [varName, value] 
